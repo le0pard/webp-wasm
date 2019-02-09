4ec33f876ccfb4997bb54988f57330dea5ef0f77
Alexey Vasiliev <leopard.not.a@gmail.com>
2019-02-09 19:12:52 +0200

    Add spinner

---
+++ webpack/components/encodeSpinner/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './encode-spinner.sass'
+
+export default class EncodeSpinner extends React.Component {
+  render() {
+    return (
+      <div className="encode-spinner-grid">
+        <div className="encode-spinner-item encode-spinner1" />
+        <div className="encode-spinner-item encode-spinner2" />
+        <div className="encode-spinner-item encode-spinner3" />
+        <div className="encode-spinner-item encode-spinner4" />
+        <div className="encode-spinner-item encode-spinner5" />
+        <div className="encode-spinner-item encode-spinner6" />
+        <div className="encode-spinner-item encode-spinner7" />
+        <div className="encode-spinner-item encode-spinner8" />
+        <div className="encode-spinner-item encode-spinner9" />
+      </div>
+    )
+  }
+}
